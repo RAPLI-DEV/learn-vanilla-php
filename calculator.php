992be6e03f6162ee5e9c5ac04033735e677d072d
rapli <rdp123szschirylw@gmail.com>
2025-11-09 15:14:52 +0700

    add percent feature and comma feature, and fixed bug result

--- calculator.php
+++ calculator.php
@@ -6,8 +6,7 @@ error_reporting(E_ALL);
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
         if(!empty(file_get_contents('php://input'))){
             $math = file_get_contents('php://input');
-
-            $formatted_math = str_replace(['÷', '×'], ['/', '*'], $math);
+            $formatted_math = str_replace(['÷', '×', '%'], ['/', '*', '/100'], $math);
             eval('$result = ' . $formatted_math . ';');
             
             http_response_code(200);
--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 class monitorValue{
     constructor() {
-       
         this.monitor = document.querySelector(".monitor")
         
         this.operators = ['+', '-', '×', '÷'];
         this.numbers = this.numbers ? this.numbers : ''
         this.o = this.o ? this.o : ""
         this.x = this.x ? this.x : ""
+        this.monitor.innerHTML = "0"
     }
     number(param){
         this.x = param
 
         this.numbers += this.x.toString()
+        this.all += this.numbers
         this.monitor.innerHTML = this.numbers
         return this.numbers = this.numbers
     }
     operation(param){
-        
         this.o = param
         this.all = this.monitor.textContent;
         let lastChar = this.all.charAt(this.all.length - 1);
 
         if (this.all.trim() === "" || this.operators.includes(lastChar)){
             return;
-        }       
+        }
         this.monitor.innerHTML = this.numbers + this.o
         return this.numbers = this.numbers + this.o
     }
     result(param){
         this.all = this.monitor.textContent;
+        console.log("result :" + this.all)
         let lastChar = this.all.charAt(this.all.length - 1);
 
         if (this.all.trim() === "" || this.o === "" || this.operators.includes(lastChar)){
             return;
-        }       
+        }
 
-        console.log("result :" + this.all)
         this.o = param
         this.monitor.innerHTML = this.o
         var xml = new XMLHttpRequest(); 
@@ -46,11 +46,11 @@ class monitorValue{
                 this.answer = xml.responseText; 
                 this.monitor.innerHTML = this.answer;
                 this.all = ""
+                return this.numbers = xml.responseText
             }
             else { console.error('Error:', xml.status, xml.statusText); }
         }
         xml.send(this.all);
-        return this.numbers = ''
     }
     reset(){
         this.monitor.innerHTML = ''
@@ -59,12 +59,13 @@ class monitorValue{
     backspace(){
         this.numbers = this.monitor.textContent.slice(0, -1)
         this.monitor.innerHTML = this.numbers
+        if (this.numbers === ""){this.monitor.innerHTML = "0"}
     }
 }
 
 const mv = new monitorValue()
 
-// document.querySelector(".np").addEventListener("click", () => {mv)})
+document.querySelector(".nP").addEventListener("click", () => {mv.operation('%')})
 document.querySelector(".nC").addEventListener("click", () => {mv.reset()})
 document.querySelector(".nbs").addEventListener("click", () => {mv.backspace()})
 document.querySelector(".np").addEventListener("click", () => {mv.operation('+')})
@@ -81,31 +82,5 @@ document.querySelector(".n2").addEventListener("click", () => {mv.number(2)})
 document.querySelector(".n3").addEventListener("click", () => {mv.number(3)})
 document.querySelector(".nm").addEventListener("click", () => {mv.operation('-')})
 document.querySelector(".n0").addEventListener("click", () => {mv.number(0)})
-// document.querySelector(".nc").addEventListener("click", () => {mv)})
+document.querySelector(".nc").addEventListener("click", () => {mv.number('.')})
 document.querySelector(".nr").addEventListener("click", () => {mv.result("")})
-
-
-// document.querySelector(".n%").addEventListener("click", () => {console.log("")})
-// document.querySelector(".nC").addEventListener("click", () => {console.log("")})
-// document.querySelector(".nbs").addEventListener("click", () => {console.log("")})
-// document.querySelector(".n+").addEventListener("click", () => {console.log("")})
-// document.querySelector(".n7").addEventListener("click", () => {console.log("7")})
-// document.querySelector(".n8").addEventListener("click", () => {console.log("8")})
-// document.querySelector(".n9").addEventListener("click", () => {console.log("9")})
-// document.querySelector(".n:").addEventListener("click", () => {console.log("")})
-// document.querySelector(".n4").addEventListener("click", () => {console.log("4")})
-// document.querySelector(".n5").addEventListener("click", () => {console.log("5")})
-// document.querySelector(".n6").addEventListener("click", () => {console.log("6")})
-// document.querySelector(".nx").addEventListener("click", () => {console.log("")})
-// document.querySelector(".n1").addEventListener("click", () => {console.log("1")})
-// document.querySelector(".n2").addEventListener("click", () => {console.log("2")})
-// document.querySelector(".n3").addEventListener("click", () => {console.log("3")})
-// document.querySelector(".n-").addEventListener("click", () => {console.log("")})
-// document.querySelector(".n0").addEventListener("click", () => {console.log("0")})
-// document.querySelector(".n,").addEventListener("click", () => {console.log("")})
-// document.querySelector(".n=").addEventListener("click", () => {console.log("")})
-
-
-
-
-
